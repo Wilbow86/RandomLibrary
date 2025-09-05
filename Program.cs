c283a942d7b7b909b9eaa96468ceb7966b8a105d
Wilbow86 <e5williams@ucsd.edu>
2025-09-04 19:34:46 -0700

    cleaned executable

--- Program.cs
+++
@@ -1,8 +0,0 @@
-﻿using System;
-using static RandLib.CategoricalDist.CategoricalDist;
-
-//tests
-
-string result = flip(0.5) ? "heads" : "tails"; 
-
-Console.WriteLine("flipped and got " + result + ".");
--- RandLib/CategoricalDist/CategoricalDist.cs
+++ RandLib/CategoricalDist/CategoricalDist.cs
@@ -1,20 +1,20 @@
 namespace RandLib.CategoricalDist
 {
-    public class CategoricalDist
-    {
-        //gets a random number between 0 and 1 then subtracts it from pHeads. True if nonnegative
-        public static bool flip(double pHeads)
-        {
-            Random rng = new Random();
-            double num = rng.NextDouble();
-            return (pHeads - num) >= 0;
-        }
-        //seeded version of above, returns modified seed
-        public static (bool, int) flip(double pHeads, int seed)
-        {
-            Random rng = new Random(seed);
-            double num = rng.NextDouble();
-            return ( pHeads >= num, rng.Next());
-        }
-    }
+	public class CategoricalDist
+	{
+		//gets a random number between 0 and 1 then subtracts it from pHeads. True if nonnegative
+		public static bool flip(double pHeads)
+		{
+			Random rng = new Random();
+			double num = rng.NextDouble();
+			return (pHeads - num) >= 0;
+		}
+		//seeded version of above, returns modified seed
+		public static (bool, int) flip(double pHeads, int seed)
+		{
+			Random rng = new Random(seed);
+			double num = rng.NextDouble();
+			return ( pHeads >= num, rng.Next());
+		}
+	}
 }
